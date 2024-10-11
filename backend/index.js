192a7835d27900a1b9a129257f76bfef404f267f
Vishal <vishaljoshi1502@gmail.com>
2024-10-11 20:26:10 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.get('/',(req,res)=>{
     res.send("Hello Worldd!")
 })
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin", `*`);
+    res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
     // Handle preflight OPTIONS requests
