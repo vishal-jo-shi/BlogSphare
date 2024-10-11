afc29e3b319a4618c8568da269a63871ca5b9ba3
Vishal <vishaljoshi1502@gmail.com>
2024-10-11 20:40:18 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 const app = express()
 
 mongoDB();
-app.get('/',(req,res)=>{
-    res.send("Hello Worldd!")
-})
+
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -21,6 +19,9 @@ app.use((req,res,next)=>{
     );
     next();
   })
+  app.get('/',(req,res)=>{
+    res.send("Hello Worldd!")
+})
   app.use(express.json())
   app.use(express.urlencoded({ extended: true })); 
   app.use('/api',require("./Routes/CreateUser"))
