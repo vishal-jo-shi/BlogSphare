07360af7f4cf3a8c2b51077b299c2029d97ea2ab
Vishal <vishaljoshi1502@gmail.com>
2024-10-11 19:30:18 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -2,39 +2,11 @@ const express = require('express')
 const {mongoDB} = require('./database/db')
 require('dotenv').config();
 const app = express()
-const { fetchBlogs, fetchCategories, fetchComments } = require('./database/fetchData');
 
 mongoDB();
 app.get('/',(req,res)=>{
     res.send("Hello Worldd!")
 })
-//testing
-app.get('/blogs', async (req, res) => {
-  try {
-      const blogs = await fetchBlogs();
-      res.json(blogs);
-  } catch (error) {
-      res.status(500).json({ message: 'Error fetching blogs', error });
-  }
-});
-
-app.get('/categories', async (req, res) => {
-  try {
-      const categories = await fetchCategories();
-      res.json(categories);
-  } catch (error) {
-      res.status(500).json({ message: 'Error fetching categories', error });
-  }
-});
-
-app.get('/comments', async (req, res) => {
-  try {
-      const comments = await fetchComments();
-      res.json(comments);
-  } catch (error) {
-      res.status(500).json({ message: 'Error fetching comments', error });
-  }
-});
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
