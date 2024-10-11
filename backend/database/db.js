fdba5ee063c595a890258f887ffca37a9fbfe5a1
Vishal <vishaljoshi1502@gmail.com>
2024-10-11 20:50:46 +0530

    updated files

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const mongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        isConnected = true; // Update connection status
         console.log('Connected to MongoDB');
     } catch (error) {
         console.log('Connection failed', error);
