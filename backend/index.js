941aee7f33c9b16ca878fd4e4462a93ec13d39b0
Vishal <vishaljoshi1502@gmail.com>
2024-10-12 17:05:29 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Middleware for CORS
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
+    res.setHeader("Access-Control-Allow-Origin", `*`);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     // Handle preflight OPTIONS requests
