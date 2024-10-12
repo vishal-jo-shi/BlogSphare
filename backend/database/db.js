5d698acf90f02b11a509a499336c4fbe30d79430
Vishal <vishaljoshi1502@gmail.com>
2024-10-12 12:59:15 +0530

    updated files

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,25 +5,21 @@ const mongoDB = async () => {
     try {
         // Connect to MongoDB
         await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 20000 // Increase timeout to 20 seconds
         });
-        
         console.log('Connected to MongoDB');
 
-        // Fetching collections
-        const fetch_blogs = mongoose.connection.collection("blogs");
-        const fetch_cat = mongoose.connection.collection("blog_category");
-        const fetch_cmt = mongoose.connection.collection("comments");
-
-        // Use Promise.all to fetch all data concurrently
-        [global.blogs, global.blogsCat, global.blogsCmt] = await Promise.all([
-            fetch_blogs.find().toArray(),
-            fetch_cat.find().toArray(),
-            fetch_cmt.find().toArray(),
+        // Fetching data concurrently
+        const [blogs, blogsCat, blogsCmt] = await Promise.all([
+            mongoose.connection.db.collection("blogs").find().toArray(),
+            mongoose.connection.db.collection("blog_category").find().toArray(),
+            mongoose.connection.db.collection("comments").find().toArray()
         ]);
 
-        console.log('Fetched blogs, categories, and comments successfully');
+        global.blogs = blogs;
+        global.blogsCat = blogsCat;
+        global.blogsCmt = blogsCmt;
+
     } catch (error) {
         console.log('Connection failed', error);
     }
