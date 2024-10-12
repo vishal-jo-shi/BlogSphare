5efac46cc2360857cec2d91f6520840d50469591
Vishal <vishaljoshi1502@gmail.com>
2024-10-12 13:17:10 +0530

    updated files

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 const mongoDB = async () => {
     try {
         // Connect to MongoDB
-        await mongoose.connect(process.env.MONGO_URL, {
+        const db = await mongoose.connect(process.env.MONGO_URL, {
             serverSelectionTimeoutMS: 20000 // Increase timeout to 20 seconds
         });
         console.log('Connected to MongoDB');
 
         // Fetching data concurrently
         const [blogs, blogsCat, blogsCmt] = await Promise.all([
-            mongoose.connection.db.collection("blogs").find().toArray(),
-            mongoose.connection.db.collection("blog_category").find().toArray(),
-            mongoose.connection.db.collection("comments").find().toArray()
+            db.collection("blogs").find().toArray(),
+            db.collection("blog_category").find().toArray(),
+            db.collection("comments").find().toArray()
         ]);
 
         global.blogs = blogs;
